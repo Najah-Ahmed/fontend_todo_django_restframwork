03e0225ae0300e413bf973da28a838894a64c4fc
najah said <najaah8800@gmail.com>
2021-01-06 10:24:37 +0300

    intial commit Forntend for django-restframework-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+const todoItems = [
+  {
+    id: 1,
+    title: "Go to Market",
+    description: "Buy ingredients to prepare dinner",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Study",
+    description: "Read Algebra and History textbook for upcoming test",
+    completed: false
+  },
+  {
+    id: 3,
+    title: "Sally's books",
+    description: "Go to library to rent sally's books",
+    completed: true
+  },
+  {
+    id: 4,
+    title: "Article",
+    description: "Write article on how to use django with react",
+    completed: false
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Hello</h1>
     </div>
   );
 }
